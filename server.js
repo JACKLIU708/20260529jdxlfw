fd07b1522d5116d1a3677bed3b08dac7cfa66d3f
JACKLIU708 <lyx623521@gmail.com>
2026-09-21 09:32:24 +0800

    Embed professional Chinese UI typography

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const types = {
   ".css": "text/css; charset=utf-8",
   ".js": "application/javascript; charset=utf-8",
   ".png": "image/png",
+  ".woff2": "font/woff2",
   ".md": "text/markdown; charset=utf-8",
 };
 
